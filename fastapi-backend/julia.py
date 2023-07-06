5477a6a9972e3f4514b54bc53baa8a1bfbb2a982
Mo Amer <mamer@Mos-MacBook-Air.local>
2023-07-06 17:54:30 +0100

    updated data

---
+++ fastapi-backend/julia.py
@@ -0,0 +1,37 @@
+# from the top, we can generate text using a simple completion prompt
+# does this have contexT?
+
+import openai
+import os
+from dotenv import load_dotenv
+
+# load up the .env file
+load_dotenv()
+
+# Set your OpenAI API Key
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def get_gpt3_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.7,
+        top_p=1
+    )
+    return response.choices[0].text.strip()
+
+
+def chat():
+    print("Julia: Hi, I'm Julia. Your personal AI assistant!")
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() == 'quit':
+            break
+        response = get_gpt3_response(user_input)
+        print("Julia:", response)
+
+
+if __name__ == '__main__':
+    chat()
